dbb8a4a9f085ca94ffb2fede9f45c6ea2303114e
HosseinGhDixper <71760464+HosseinGhDixper@users.noreply.github.com>
2021-09-10 13:29:23 +0200

    Improve question documentation.

--- question-a/car-decelerating-position.cpp
+++ question-a/car-decelerating-position.cpp
@@ -3,11 +3,11 @@
 namespace QuestionA
 {
   /**
-   * Given a sequence of positions of a car that is accelerating at first and then starts decelerating, finds the
-   * position at which the car started decelerating.
+   * Given a sequence of positions of a car that is accelerating at first and then starts decelerating and keeps
+   * decelerating to the end, finds the position at which the car started decelerating.
    *
    * \param car_position_data Contains a sequence of numbers (at least two) that represents position of the car sampled
-   * at consequitive time intervals. The car is accelerating at the first position and decelerating at the last
+   * at consequitive equal time intervals. The car is accelerating at the first position and decelerating at the last
    * position. (Speed of the car is positive at the start)
    *
    * \return The first position in the input at which the car starts decelerating.
